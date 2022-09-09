e2a2b7b2305121767b8e764043557148ecce3145
mciethan <emaclex@gmail.com>
2022-09-09 19:02:54 -0400

    joined attributes to geometry and printed join info

--- test.py
+++ test.py
@@ -1,9 +1,19 @@
 import geopandas as gpd
+import pandas as pd
 import os
 
 attrs_file = 'data/parcel_attributes/Boston_Property_Assessment_2021.csv'
 geoms_path = 'data/parcel_geometries/Boston_Parcels_2021.geojson'
 
 gdf = gpd.read_file(geoms_path)
-print(gdf.columns)
+num_records_before = len(gdf)
 
+attrs_df = pd.read_csv(attrs_file, usecols=['PID', 'ST_NAME'], dtype=str)
+attrs_df = attrs_df.rename(columns={"PID": "MAP_PAR_ID"})
+
+gdf = gdf.merge(attrs_df, on='MAP_PAR_ID')
+num_records_after = len(gdf)
+
+print("Of the " + str(num_records_before) + " records in the geometry file, "
+    + str(num_records_after) + " were successfully joined to parcel attributes (" 
+    + str(round(num_records_after*100/num_records_before, 1)) + "%).")
